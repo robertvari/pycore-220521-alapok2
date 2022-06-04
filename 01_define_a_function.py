2318c2fe2384c00f0d38960095730cfcb85f68ef
Robert Vári <mail.robertvari@gmail.com>
2022-06-04 14:19:00 +0200

    init

---
+++ 01_define_a_function.py
@@ -0,0 +1,19 @@
+# define a function
+def say_hello1():
+    print("Hello World")
+
+
+def say_hello2():
+    print("Hello World 2")
+
+
+def say_hello3():
+    print("Hello World 3")
+
+
+# call a function
+say_hello3()
+say_hello1()
+say_hello2()
+say_hello3()
+say_hello1()
