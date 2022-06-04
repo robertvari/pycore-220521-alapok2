55320a115ede5e52d3526e4fe422641c8dc2aac2
Robert Vári <mail.robertvari@gmail.com>
2022-06-04 17:20:03 +0200

    update

--- utilities/file_utils.py
+++ utilities/file_utils.py
@@ -10,4 +10,20 @@ def get_all_files(root_folder: str, file_list: list):
     :return: None
     """
 
+    # get all content from root_folder (files, folders)
     folder_content = [os.path.join(root_folder, i) for i in os.listdir(root_folder)]
+
+    # collect files
+    for i in folder_content:
+        if os.path.isfile(i):
+            # todo use some kind of filter (name, extension)?
+            file_list.append(i)
+
+    # get subfolders
+    subfolders = []
+    for i in folder_content:
+        if os.path.isdir(i):
+            subfolders.append(i)
+
+    for folder in subfolders:
+        get_all_files(folder, file_list)
