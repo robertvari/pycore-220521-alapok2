85ab683f6ff4ec5c1752ef3c01bbe07f58c37842
Robert Vári <mail.robertvari@gmail.com>
2022-06-04 15:30:53 +0200

    init

---
+++ 06_modules.py
@@ -0,0 +1,7 @@
+from utilities.math_utils import add_numbers, divide_numbers, multiply_numbers
+# from testing import * bad practice
+
+
+print(add_numbers(2, 2))
+print(divide_numbers(2, 2))
+print(multiply_numbers(2, 2))
---
+++ testing.py
@@ -0,0 +1,5 @@
+my_name = "Robert"
+
+
+def say_hello():
+    print("Hello from testing.py")
